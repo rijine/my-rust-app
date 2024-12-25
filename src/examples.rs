b178f43faca871e9355bf2b43192a2acd9a001f2
Rijin Elayambari <e.rijin@gmail.com>
2024-12-25 13:46:08 +0000

    Adding axum and tokio

---
+++ src/examples.rs
@@ -0,0 +1,55 @@
+
+pub fn structs() {
+    struct Pair(i32, i32);
+
+    struct Rectangle {
+        top_left: Pair,
+        bottom_right: Pair,
+    }
+
+    let rect: Rectangle = Rectangle{top_left: Pair(1,2), bottom_right: Pair(3,4)};
+
+    let Rectangle { top_left: Pair{0: x, 1: y}, bottom_right: right}  = rect;
+
+    print!("{} {} ", x, y);
+
+}
+
+pub enum Event {
+    PageX,
+    Key(String)
+}
+
+impl Event {
+    fn run(&self, x: i32, y: i32) -> i32 {
+        match self {
+            Self::PageX => x + y,
+            _ => 0
+        }
+    }
+}
+
+pub fn enums() {
+    fn inpsect(event: Event) {
+        match event {
+            Event::Key(st) => println!("{}", st),
+            _ => println!("missing")
+        }
+    }
+
+    type XMen = Event;
+
+    inpsect(Event::Key(String::from("EVENT_KEY1")));
+    inpsect(XMen::Key("EVENT_KEY2".to_owned()));
+    inpsect(XMen::PageX);
+
+    println!("{}", XMen::PageX.run(3, 4));
+
+    let s1 = "123".to_owned();
+    let s2 = String::from("456");
+    let s3 = "789";
+
+    let s4 = s1 + &s2 + s3;
+    print!("{}",  s4);
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use axum::response::Html;
+use axum::{Router, ServiceExt};
+use axum::routing::get;
+use crate::examples::{enums, structs};
+
+mod examples;
 
 
-    structs();
+#[tokio::main]
+async fn main() {
+    let routes = Router::new()
+        .route("/hello", get(|| async { Html("Hello, world!") }));
 
-    enums();
-}
 
-fn structs() {
-    struct Pair(i32, i32);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("listening on {}", addr);
+    axum::serve(listener, routes.into_make_service()).await.unwrap();
 
-    struct Rectangle {
-        top_left: Pair,
-        bottom_right: Pair,
-    }
-
-    let rect: Rectangle = Rectangle{top_left: Pair(1,2), bottom_right: Pair(3,4)};
-
-    let Rectangle { top_left: Pair{0: x, 1: y}, bottom_right: right}  = rect; 
-
-    print!("{} {} ", x, y);
-
-}
-
-enum Event {
-    PageX,
-    Key(String)
-}
-
-impl Event {
-    fn run(&self, x: i32, y: i32) -> i32 {
-        match self {
-            Self::PageX => x + y,
-            _ => 0
-        }
-    }
-}
-
-fn enums() {
-    fn inpsect(event: Event) {
-        match event {
-            Event::Key(st) => println!("{}", st),
-            _ => println!("missing")
-        }
-    }
-
-    type XMen = Event;
-
-    inpsect(Event::Key(String::from("EVENT_KEY1")));
-    inpsect(XMen::Key("EVENT_KEY2".to_owned()));
-    inpsect(XMen::PageX);
-
-    println!("{}", XMen::PageX.run(3, 4));
-
-    let s1 = "123".to_owned();
-    let s2 = String::from("456");
-    let s3 = "789";
-
-    let s4 = s1 + &s2 + s3;
-    print!("{}",  s4);
-
+    // Tryouts
+    // structs();
+    // enums();
 }
