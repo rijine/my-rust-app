04e01a5e247763fa48f5d126fd9005a66b6d987c
Rijin Elayambari <e.rijin@gmail.com>
2024-12-25 14:04:54 +0000

    query exractor - deref to access without new memory allocation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::net::SocketAddr;
-use axum::response::Html;
+use axum::response::{Html, IntoResponse};
 use axum::{Router, ServiceExt};
+use axum::extract::Query;
 use axum::routing::get;
+use serde::Deserialize;
 use crate::examples::{enums, structs};
 
 mod examples;
@@ -10,7 +12,7 @@ mod examples;
 #[tokio::main]
 async fn main() {
     let routes = Router::new()
-        .route("/hello", get(|| async { Html("Hello, world!") }));
+        .route("/hello", get(handler_hello));
 
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -21,4 +23,16 @@ async fn main() {
     // Tryouts
     // structs();
     // enums();
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("--> GET /hello {:<12} - hello handler {params:?}", "HANDLER");
+    let name = params.name.as_deref().unwrap_or("World!");
+
+    Html(format!("Hello, {name}!"))
 }
--- tests/hello_test.rs
+++ tests/hello_test.rs
@@ -3,6 +3,6 @@ use anyhow::Result;
 #[tokio::test]
 async fn hello() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
-    hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello?name=Rocky").await?.print().await?;
     Ok(())
 }
