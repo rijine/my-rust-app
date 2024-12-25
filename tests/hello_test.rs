ab11efc67862d368878ada3e975ac6c7171227be
Rijin Elayambari <e.rijin@gmail.com>
2024-12-25 13:52:54 +0000

    adding http test

---
+++ tests/hello_test.rs
@@ -0,0 +1,8 @@
+use anyhow::Result;
+
+#[tokio::test]
+async fn hello() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:8080")?;
+    hc.do_get("/hello").await?.print().await?;
+    Ok(())
+}
